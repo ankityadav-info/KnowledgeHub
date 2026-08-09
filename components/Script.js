6e89f42ae2fb1f149a42437e05da4b9db4618e16
Ankit Yadav <36183104+ankityadav-info@users.noreply.github.com>
2026-08-09 14:04:45 +0530

    Add files via upload

---
+++ components/Script.js
@@ -0,0 +1,53 @@
+var header = "<div style='text-transform: uppercase;' align='center'><a class='white' href='https://www.knowledgekahub.com/p/knowledge-hub.html'><span style='color:#4285f4;'>K</span><span style='color:#ea4335;'>n</span><span style='color:#fbbc05;'>o</span><span style='color:#34a853;'>w</span><span style='color:#4285f4;'>l</span><span style='color:#ea4335;'>e</span><span style='color:#fbbc05;'>d</span><span style='color:#34a853;'>g</span><span style='color:#4285f4;;'>e</span><span style='font-family: Kalam , cursive;color:#34a853;'>का</span><span style='color:#ea4335;'>H</span><span style='color:#fbbc05;'>u</span><span style='color:#34a853;'>b</span></a></div>";
+document.getElementById("header").innerHTML = header;
+
+var footer = "<table style='border:none;'><tr class='rb'><td class='cb rbb'></td><td class='cr rbb'></td><td class='cy rbb'></td><td class='cg rbb'></td></tr></table><div class='links' align='center'><p id='disclaimer'>Disclaimer - यह पीडीएफ विभिन्न स्रोतों से तथ्य एकत्रित करके बनायीं गयी है , यदि इसमें कोई त्रुटि पायी जाती है तो नॉलेज हब संचालक की जिम्मेदारी नहीं होगी ।</p><a target='blank' class='linksfoot' href='https://www.knowledgekahub.com/p/knowledge-hub.html'>अन्य पीडीएफ डाउनलोड करने के लिए यहाँ क्लिक करें या  गूगल पर सर्च करें - knowledgekahub</a></div><div align='center' class='footer'><p id='bottamtext'>Download Free Study Notes From Website</p></div>";
+document.getElementById("footer").innerHTML = footer;
+
+/*
+<a class='white' href='https://www.knowledgekahub.com/p/knowledge-hub.html'>www.knowledgekahub.com</a><br /><a href='https://t.me/joinchat/AAAAAEstkGqjH0SmOIHe3Q' class='fa fa-telegram'></a><a href='https://www.knowledgekahub.com/p/whatsapp-group-knowledge-hub.html' class='fa fa-whatsapp'></a><a href='https://facebook.com/knowledgekahub/' class='fa fa-facebook'></a><a href='https://youtube.com/c/knowledgehubkmshub' class='fa fa-youtube'></a><a href='https://twitter.com/knowledgekahub' class='fa fa-twitter'></a>
+<script type='text/javascript'>
+var isCtrl = false;
+document.onkeyup=function(e)
+{
+if(e.which == 17)
+isCtrl=false;
+}
+document.onkeydown=function(e)
+{
+if(e.which == 123)
+isCtrl=true;
+if (((e.which == 85) || (e.which == 65) || (e.which == 88) || (e.which == 67) || (e.which == 86) || (e.which == 2) || (e.which == 3) || (e.which == 123) || (e.which == 83)) && isCtrl == true)
+{
+alert('This Content Is Copyrighted.');
+return false;
+}
+}
+// right click code
+var isNS = (navigator.appName == "Netscape") ? 1 : 0;
+if(navigator.appName == "Netscape") document.captureEvents(Event.MOUSEDOWN||Event.MOUSEUP);
+function mischandler(){
+    alert('This Content Is Copyrighted.');
+return false;
+}
+function mousehandler(e){
+var myevent = (isNS) ? e : event;
+var eventbutton = (isNS) ? myevent.which : myevent.button;
+if((eventbutton==2)||(eventbutton==3)) return false;
+}
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
+//select content code disable
+function killCopy(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=killCopy
+document.onclick=reEnable
+}
+</script>*/
