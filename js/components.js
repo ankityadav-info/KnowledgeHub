29727f2b9f35418410b1bddca8d652ae1bab4946
Ankit Yadav <36183104+ankityadav-info@users.noreply.github.com>
2026-08-09 13:51:17 +0530

    uploaded files

---
+++ js/components.js
@@ -0,0 +1,11 @@
+async function loadComponent(id, file) {
+    const element = document.getElementById(id);
+
+    if (element) {
+        const response = await fetch(file);
+        element.innerHTML = await response.text();
+    }
+}
+
+loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
